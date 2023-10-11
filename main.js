7070af0d72d8c95553b06513f31ea79132c33059
dostonwebdev <dostone320@gmail.com>
2023-10-11 13:29:29 +0500

    home work

---
+++ main.js
@@ -0,0 +1,33 @@
+// for 1
+
+// let k = parseInt(prompt("k sonini kiriting: "));
+// let n = parseInt(prompt("n sonini kiriting: "));
+
+// for (let i = 0; i < n; i++) {
+//     console.log(k);
+// }
+
+// for 4
+
+// let narxPerKg = 12000;
+// let kg = 10;
+
+// let narx = narxPerKg * kg;
+// console.log(narx);
+
+// for 10
+// let n = prompt("n kiriting");
+// let s = 1 + 1 / 2 + 1 / 3 + n + 1 / n;
+// console.log(s);
+
+// for 11
+
+// let n = prompt("n kiriting");
+// let s = n ** 2 + (n + 1) ** 2 + (n + 2) ** 2 + n + (2 * n) ** 2;
+// console.log(s);
+
+// f0r 12
+
+// let n = prompt("n kiriting");
+// let s = 1.1 * 1.2 * 1.3 * n;
+// console.log(s);
